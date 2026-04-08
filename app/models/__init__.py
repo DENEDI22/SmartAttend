9943cfeadbc2008f55956ac5137fdc46851347fe
Danylo Liubymyi <denedi2004@gmail.com>
2026-04-08 10:37:15 +0200

    feat(12-01): create SystemSetting model and add late_threshold_minutes to ScheduleEntry
    - Add SystemSetting key-value model with get_value/set_value helpers
    - Add nullable late_threshold_minutes column to ScheduleEntry
    - Wire SystemSetting import in models __init__

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,6 @@ from app.models.schedule_entry import ScheduleEntry
 from app.models.attendance_token import AttendanceToken
 from app.models.attendance_record import AttendanceRecord
 from app.models.school_class import SchoolClass
+from app.models.system_setting import SystemSetting
 
-__all__ = ["User", "Device", "ScheduleEntry", "AttendanceToken", "AttendanceRecord", "SchoolClass"]
+__all__ = ["User", "Device", "ScheduleEntry", "AttendanceToken", "AttendanceRecord", "SchoolClass", "SystemSetting"]
